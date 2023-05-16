6357a58a5a758b2c94a7fc6e7fdc7dff00211904
kasivisu4 <kasivisu4@gmail.com>
2023-05-16 21:08:15 +0530

    added the users accessible for the file

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 //Reference Link: https://developers.google.com/drive/api/quickstart/js
-//Update CLIENT_ID and API key from the developer console
-const CLIENT_ID = process.env.CLIENT_ID;
-const API_KEY = process.env.API_KEY;
+//Change If needed CLIENT_ID and API key from the developer console
+const CLIENT_ID =
+  "276639897094-d27mg95pcja2n2ubkld74us0373h1q86.apps.googleusercontent.com";
+const API_KEY = "AIzaSyAT7CmyITspnXjLWVPzXOvguKBt0CJZYlQ";
 
 // Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC =
   "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest";
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
 const SCOPES = "https://www.googleapis.com/auth/drive";
 
 let tokenClient;
@@ -17,18 +16,13 @@ let gisInited = false;
 
 document.getElementById("authorize_button").style.visibility = "hidden";
 document.getElementById("signout_button").style.visibility = "hidden";
+document.getElementById("download_button").style.visibility = "hidden";
+document.getElementById("status").innerHTML = "Not Connected ⚠️";
 
-/**
- * Callback after api.js is loaded.
- */
 function gapiLoaded() {
   gapi.load("client", initializeGapiClient);
 }
 
-/**
- * Callback after the API client is loaded. Loads the
- * discovery doc to initialize the API.
- */
 async function initializeGapiClient() {
   await gapi.client.init({
     apiKey: API_KEY,
@@ -38,31 +32,22 @@ async function initializeGapiClient() {
   maybeEnableButtons();
 }
 
-/**
- * Callback after Google Identity Services are loaded.
- */
 function gisLoaded() {
   tokenClient = google.accounts.oauth2.initTokenClient({
     client_id: CLIENT_ID,
     scope: SCOPES,
-    callback: "", // defined later
+    callback: "",
   });
   gisInited = true;
   maybeEnableButtons();
 }
 
-/**
- * Enables user interaction after all libraries are loaded.
- */
 function maybeEnableButtons() {
   if (gapiInited && gisInited) {
     document.getElementById("authorize_button").style.visibility = "visible";
   }
 }
 
-/**
- *  Sign in the user upon button click.
- */
 function handleAuthClick() {
   tokenClient.callback = async (resp) => {
     if (resp.error !== undefined) {
@@ -71,22 +56,18 @@ function handleAuthClick() {
     console.log(resp);
     document.getElementById("signout_button").style.visibility = "visible";
     document.getElementById("authorize_button").innerText = "Refresh";
+    document.getElementById("status").innerHTML = "Connected 🟢";
+
     await listFiles();
   };
 
   if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
     tokenClient.requestAccessToken({ prompt: "consent" });
   } else {
-    // Skip display of account chooser and consent dialog for an existing session.
     tokenClient.requestAccessToken({ prompt: "" });
   }
 }
 
-/**
- *  Sign out the user upon button click.
- */
 function handleSignoutClick() {
   const token = gapi.client.getToken();
   if (token !== null) {
@@ -95,24 +76,25 @@ function handleSignoutClick() {
     document.getElementById("details").innerText = "";
     document.getElementById("authorize_button").innerText = "Authorize";
     document.getElementById("signout_button").style.visibility = "hidden";
+    document.getElementById("status").innerHTML = "Disconnected ❌";
+    document.getElementById("download_button").style.visibility = "hidden";
   }
 }
 
-/**
- * Print metadata for first 10 files.
- */
 async function listFiles() {
   let response;
   try {
     response = await gapi.client.drive.files.list({
       pageSize: 10,
-      fields: "files(id, name,webContentLink)",
+      fields: "files(id,name,webContentLink,permissions(emailAddress))",
     });
   } catch (err) {
     document.getElementById("details").innerText = err.message;
     return;
   }
   const files = response.result.files;
+  console.log(files);
+
   if (!files || files.length == 0) {
     document.getElementById("details").innerText = "No files found.";
     return;
@@ -122,60 +104,100 @@ async function listFiles() {
     "details"
   ).innerText = `Total Files : ${files.length}`;
 
+  // document.getElementById("download_button").style.visibility = "visible";
+
   let renderDrive = document.getElementById("driveDetails");
 
+  let keys = ["name", "emailAddress", "webContentLink", "id"];
+
+  let table = document.createElement("table");
+  table.className = "table";
+  // Create the table header
+  let thead = document.createElement("thead");
+  thead.className = "thead-dark";
+  let headerRow = document.createElement("tr");
+  ["File Name", "Accessible By", "Download Link", "id"].forEach(function (key) {
+    let th = document.createElement("th");
+    th.textContent = key;
+    headerRow.appendChild(th);
+  });
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  let tbody = document.createElement("tbody");
   files.forEach((file) => {
-    let div = document.createElement("div");
-    let input = document.createElement("input");
-    input.type = "checkbox";
-    input.value = file.webContentLink;
-    input.className = "selectedFiles";
-
-    let label = document.createElement("label");
-    label.appendChild(document.createTextNode(file.name));
-    div.appendChild(input);
-    div.appendChild(label);
-
-    renderDrive.appendChild(div);
-  });
-
-  let downloadButton = document.getElementById("download");
-  downloadButton.addEventListener("click", async () => {
-    let selectedIds = [];
-    let selectedFiles = Array.from(document.querySelectorAll(".selectedFiles"));
-    for (let node of selectedFiles) {
-      if (node.checked) {
-        selectedIds.push(node.value);
+    let tr = document.createElement("tr");
+    tr.style = "max-width:400px";
+    keys.forEach(function (value) {
+      let td = document.createElement("td");
+      td.style = "max-width:400px;overflow-wrap: break-word;";
+      if (value === "emailAddress") {
+        td.textContent = file["permissions"]?.map((d) => d[value]);
+      } else if (value === "id") {
+        let btn = document.createElement("button");
+        btn.innerText = file.id;
+        btn.addEventListener("click", async (evt) => {
+          console.log("event", evt);
+          console.log("id", file.id);
+          await download(file.id);
+        });
+        td.appendChild(btn);
+      } else {
+        td.textContent = file[value] || "-";
       }
-    }
-
-    //Download the files
-    if (selectedIds.length === 0) {
-      console.log("No files selected for download.");
-      return;
-    }
-
-    // Iterate through the selected file IDs and download each file
-    for (let fileLink of selectedIds) {
-      console.log(fileLink);
-      try {
-        const element = document.createElement("a");
-        element.href = fileLink;
-        element.style.display = "none";
-        element.click();
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-
-        // Perform further actions with the downloaded file data
-        // For example, you can save the file locally or process it in some way.
-      } catch (err) {
-        console.error("Error downloading file:", err);
-      }
-    }
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
   });
+  table.appendChild(tbody);
+  renderDrive.appendChild(table);
 }
 
-// // Flatten to string to display
-// const output = files.reduce(
-//   (str, file) => `${str}${file.name} (${file.id})\n`,
-//   "Files:\n"
-// );
+async function download(fileId) {
+  try {
+    const file = await gapi.client.drive.files.get({
+      fileId: fileId,
+      alt: "media",
+    });
+    debugger;
+
+    console.log(file.status);
+    return file.status;
+  } catch (err) {
+    // TODO(developer) - Handle error
+    throw err;
+  }
+}
+//   let downloadButton = document.getElementById("download_button");
+//   downloadButton.addEventListener("click", async () => {
+//     let selectedIds = [];
+//     let selectedFiles = Array.from(document.querySelectorAll(".selectedFiles"));
+//     for (let node of selectedFiles) {
+//       if (node.checked) {
+//         selectedIds.push(node.value);
+//       }
+//     }
+
+//     //Download the files
+//     if (selectedIds.length === 0) {
+//       console.log("No files selected for download.");
+//       return;
+//     }
+
+//     // Iterate through the selected file IDs and download each file
+//     for (let fileLink of selectedIds) {
+//       try {
+//         const element = document.createElement("a");
+//         element.href = fileLink;
+//         element.style.display = "none";
+//         element.click();
+//         await new Promise((resolve) => setTimeout(resolve, 5000));
+
+//         // Perform further actions with the downloaded file data
+//         // For example, you can save the file locally or process it in some way.
+//       } catch (err) {
+//         console.error("Error downloading file:", err);
+//       }
+//     }
+//   });
+// }
